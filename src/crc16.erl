deae06dc24c4d9e35711a22ba0135e04d89454c4
mmartin <mmartin@mezeo.com>
2015-01-31 16:09:45 -0600

    Base nif library off of current directory.

--- src/crc16.erl
+++ src/crc16.erl
@@ -4,7 +4,9 @@
 -on_load(init/0).
 
 init() ->
-    erlang:load_nif("./crc16", 0).
+    {ok, Cwd} = file:get_cwd(),
+    Niflib = Cwd ++ "/deps/crc16_nif/lib/crc16",
+    erlang:load_nif(Niflib, 0).
 
 crc16(_Str) ->
     "NIF library not loaded".
